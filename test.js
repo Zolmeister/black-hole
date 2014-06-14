6611928fbea3996176ca100c313e088c2aebdd2d
Zolmeister <zolikahan@gmail.com>
2014-06-14 10:26:49 -0700

    init

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict'
+var chai = require('chai')
+var expect = chai.expect
+var balckhole
+
+describe('blackhole', function () {
+  it('wraps deep objects, with an instance at each endpoint', function () {
+
+  })
+})
